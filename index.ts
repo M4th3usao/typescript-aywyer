3c54913c8bda27dd87cb2d9262cf20fa269fa98b
M4th3usao <matheus.rico17@gmail.com>
2022-08-26 19:33:39 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -11,3 +11,13 @@ appDiv.innerHTML = `
 <h2> List: ${list}</h2>
 <h2>Other List: ${otherList}</h2>
 `;
+
+let me: [String, number];
+
+me = ['Matheus', 20];
+
+const appTuple: HTMLElement = document.getElementById('app.tuple');
+appDiv.innerHTML = `
+<h1>Tuple</h1>
+<h2>Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
+`;
