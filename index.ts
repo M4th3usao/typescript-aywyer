084b18ddcd41b03b74deb2860d093ea7a62725e6
M4th3usao <matheus.rico17@gmail.com>
2022-08-26 19:42:37 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // Import stylesheets
 import './style.css';
 
+//Arrays:
 let list: number[] = [1, 2, 3];
 
 let otherList: Array<number> = [1, 2, 3];
@@ -12,6 +13,7 @@ appDiv.innerHTML = `
 <h2>Other List: ${otherList}</h2>
 `;
 
+//Tuples
 let me: [String, number];
 
 me = ['Matheus', 20];
@@ -20,4 +22,26 @@ const appTuple: HTMLElement = document.getElementById('app.tuple');
 appDiv.innerHTML = `
 <h1>Tuple</h1>
 <h2>Me chamo ${me[0]} e tenho ${me[1]} anos</h2>
+`;
+
+//Enum
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let color: Color = Color.Green;
+
+enum ColorWithRef {
+  Red = 10,
+  Green = 20,
+  Blue = 30,
+}
+let color2: ColorWithRef = ColorWithRef.Blue;
+
+const appEnum: HTMLElement = document.getElementById('app');
+appEnum.innerHTML = `
+<h1>Enumerated Types</h1>
+<h2>Cor 1: ${color}</h2>
+<h2>Cor 2: ${color2}</h2>
 `;
